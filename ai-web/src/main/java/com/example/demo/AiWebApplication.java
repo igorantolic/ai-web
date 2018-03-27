9c3c7e8b3fd3ddc2699dd0314cb0ea6df318a3cb
Igor <iantolic@gmail.com>
2018-03-27 21:29:55 +0200

    init

---
+++ ai-web/src/main/java/com/example/demo/AiWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWebApplication.class, args);
+	}
+}
---
+++ ai-web/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	  @RequestMapping("/")
+	    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	        return "bok " + name;
+	    }
+
+}
